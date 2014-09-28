768e95fdb9a36b2c8c4c61a3528f8e6183637731
Joshua Storey <jstorey@Joshuas-MacBook-Pro.local>
2014-09-28 13:14:27 -0700

    Create routes views corresponding with new and index controllers.

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -5,6 +5,18 @@ class BrandsController < ApplicationController
 	end 
 
 	def new 
-
+		@brand = Brand.new
 	end
+
+	def create
+		@brand = Brand.new(params[:new_brand])
+		if @brand.save
+			flash[:notice] = "Brand Added"
+			redirect_to('/brands')
+		else
+			render('brands/new.html.erb')
+		end
+	end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
  match('brands', {:via => :get, :to => 'brands#index'})
+ match('brands/new', {:via => :get, :to => 'brands#new'})
+ match('brands', {:via => :post, :to => 'brands#create'})
 end
