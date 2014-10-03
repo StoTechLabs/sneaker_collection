9cfb48a7104b994b5d59ac8e78bf542b30007dff
Joshua Storey <jstorey@jmba.domain>
2014-10-03 10:23:40 -0700

    Add README.md.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
- 
-
  match('/', {:via => :get, :to => 'brands#index'})
  match('brands/new', {:via => :get, :to => 'brands#new'})
  match('brands', {:via => :post, :to => 'brands#create'})
